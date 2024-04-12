9b16e0c450ab7985d97e5b5072d0094d5422ce3e
Thomas-Zangerl <thomas.zangerl@knightec.se>
2024-04-12 19:25:21 +0200

    added jenny

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,10 @@ fun main() {
     printer("JJ")
     printer("Max")
     printer("Tom")
+    printer("Jenny")
 }
 
 
-fun printer(name: String){
+fun printer(name: String) {
     println("Hello $name")
 }
