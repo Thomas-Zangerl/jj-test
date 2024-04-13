b27cd0d658fe0ac9af84666b1d25eca51d0479b1
tomz <tomzangerl@gmail.com>
2024-04-13 12:33:22 +0200

    respond to feedback

--- src/main/kotlin/Printer.kt
+++ src/main/kotlin/Printer.kt
@@ -2,12 +2,6 @@ package com.example
 
 fun printer(greeting: String, name: String) {
     println("$greeting $name")
-//"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-// dapibus feugiat sapien. Nunc accumsan odio SEM, a consectetur leo eleifend in. Curabitur eget dapibus magna.
-// Pellentesque volutpat, nulla id pretium malesuada, ligula libero ornare nisl, tempus
-// Pellentesque volutpat, nulla id pretium malesuada, ligula libero ornare nisl, tempus
-// malesuada metus eros non arcu. Quisque VEL aliquet neque. Nunc ante ligula, imperdiet ac quam non,
-// In id faucibus risus. Mauris at risus at ligula molestie mollis."
 }
 
 fun justAnotherFunction(): String{
