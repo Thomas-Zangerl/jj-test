c244d519b83dceb0fc01eadb289980f3cd9a369a
tomz <tomzangerl@gmail.com>
2024-04-13 11:55:34 +0200

    just testing less

--- src/main/kotlin/Printer.kt
+++ src/main/kotlin/Printer.kt
@@ -3,9 +3,13 @@ package com.example
 fun printer(greeting: String, name: String) {
     println("$greeting $name")
 //"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-// dapibus feugiat sapien. Nunc accumsan odio sem, a consectetur leo eleifend in. Curabitur eget dapibus magna.
+// dapibus feugiat sapien. Nunc accumsan odio SEM, a consectetur leo eleifend in. Curabitur eget dapibus magna.
 // Pellentesque volutpat, nulla id pretium malesuada, ligula libero ornare nisl, tempus
-// malesuada metus eros non arcu. Quisque vel aliquet neque. Nunc ante ligula, imperdiet ac quam non,
+// Pellentesque volutpat, nulla id pretium malesuada, ligula libero ornare nisl, tempus
+// malesuada metus eros non arcu. Quisque VEL aliquet neque. Nunc ante ligula, imperdiet ac quam non,
 // In id faucibus risus. Mauris at risus at ligula molestie mollis."
 }
 
+fun justAnotherFunction(): String{
+    return "Hello im Tom"
+}
