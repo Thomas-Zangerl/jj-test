ff55881408b6477c3e662c0ccbf7a300031f63f9
tomz <tomzangerl@gmail.com>
2024-04-13 10:42:21 +0200

    editing readme

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-hello
+hello  
+this is a readme for some greeter program
--- src/main/kotlin/Printer.kt
+++ src/main/kotlin/Printer.kt
@@ -2,4 +2,10 @@ package com.example
 
 fun printer(greeting: String, name: String) {
     println("$greeting $name")
+//"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+// Pellentesque volutpat, nulla id pretium malesuada, ligula libero ornare nisl, tempus
+// malesuada metus eros non arcu. Quisque vel aliquet neque. Nunc ante ligula, imperdiet ac quam non,
+// dapibus feugiat sapien. Nunc accumsan odio sem, a consectetur leo eleifend in. Curabitur eget dapibus magna.
+// In id faucibus risus. Mauris at risus at ligula molestie mollis."
 }
+
